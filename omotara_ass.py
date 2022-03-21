5ddce5c12b83d0aabac52dd19b5b3108f3892338
Omotara Dele-Johnson <omotaradele-johnson@Omotaras-MacBook-Air.local>
2022-03-21 10:21:21 +0000

    modifications to the bar chart

--- omotara_ass.py
+++ omotara_ass.py
@@ -24,7 +24,6 @@ print(NGA.loc['Rural_population'])
 # ,, Death_rate_crude as 'drC'
 # ,, infant_deaths as 'ID'
 data = {
-        "pol": NGA.loc['Population_total'],
         "rur": NGA.loc['Rural_population'],
         "urb": NGA.loc['Urban_population'],
         "poF": NGA.loc['Population_female'],
@@ -33,7 +32,7 @@ data = {
         "iD": NGA.loc['infant_deaths']
         }
 
-# For Bar Chart
+# plot total population and its series across the year using Bar Chart
 NGA2 = pd.DataFrame(data = data,index=NGA.columns);
 
 NGA2.plot.bar(rot = 0,title='Nigeria Population Estimate & projections');
@@ -41,8 +40,9 @@ plot.yticks([100000000,200000000,300000000])
 plot.draw();
 
 
-# For Pie Chart
+# Plot the proportion of rural and urban population across the year using Pie Chart
 
+# The Rural population plot
 pieNGA = pd.DataFrame({'population': NGA.columns,
                       'rural population': NGA.loc['Rural_population']})
 Ruralplot = plot.subplot(121, aspect='equal')
@@ -50,6 +50,7 @@ print(pieNGA)
 pieNGA.groupby(['population']).sum().plot(kind='pie',y='rural population',labels=pieNGA['population'],ax=Ruralplot, autopct='%1.1f%%', 
  startangle=90, shadow=False,fontsize=8,legend = False)
 
+# The Urban population plot
 pieNGA2 = pd.DataFrame({'population_year': NGA.columns,
                       'urban population': NGA.loc['Urban_population']})
 Urbanplot = plot.subplot(121, aspect='equal')
@@ -59,13 +60,11 @@ pieNGA2.groupby(['population_year']).sum().plot(kind='pie',y='urban population',
 
 
 
-# For LINE Graph
+# Plot the female & male population proportion using a LINE Graph
+# the female population Analysis
 Year = NGA.columns
 female_pop = NGA.loc['Population_female']
-
-
 plot.plot(Year, female_pop,label="female population")
-
 plot.xlabel('Year')
 plot.ylabel('Population')
 plot.title('NGA Female population projections')
@@ -73,7 +72,7 @@ plot.xticks(Year)
 plot.yticks([90000000,100000000,110000000])
 #plot.show()
 
-
+# the emale population Analysis
 male_pop = NGA.loc['Population_male']
 print(male_pop)
 
